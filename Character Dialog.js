a9f5d646fb97a4d5a63ac2df31ecf503ac74dd16
xaukael <37848032+xaukael@users.noreply.github.com>
2022-03-01 17:10:42 -0600

    Update Character Dialog.js

--- Character Dialog.js
+++ Character Dialog.js
@@ -1,5 +1,5 @@
 let update = await game.macros.getName("Update Macro From Git").execute(this.name);
-if (update.match) return this.execute();
+if (!update.match) return this.execute();
 
 if (!Hooks._hooks.preCreateChatMessage.filterIndex(f=>f.toString().includes('chatmessagetargetflags'))[0])
   Hooks.on(`preCreateChatMessage`, async (message, data, options, user) => {
