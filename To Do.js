f492c8bb4649697425b1910871dd3bbcecb9f828
xaukael <37848032+xaukael@users.noreply.github.com>
2022-03-09 09:39:20 -0600

    Update To Do.js

--- To Do.js
+++ To Do.js
@@ -1,10 +1,11 @@
 /*
 
-# To Do
+add images folder for screenshots of macros
+
 Actor Menu
   header button for Actor Menu to toggle horizontal and vertical layout and set user flag for preference
 
-add images folder for screenshots of macros
+
 
 git download 
   put into folder
@@ -16,4 +17,12 @@ let macros = [...]
 for (let macro of macros)
   toFile(macro.data.name+'.js') ... macro.command.replace("game.macros.getName(`macroName`).execute", `scope.functionName`, `${scope}.${macro.data.name.replace(' ', '')}`)
 
+taskbar macro or just make a module
+  get hbs template file for app from github?
+  hide player and macro bar elements by id in style
+  re-populate open windows by hooking all application type renders?
+  start menu : populated from macro hotbar.
+  users in the notification area
+  copy calendar tool code from control bar
+  taskbar color based on pause state?
 */
