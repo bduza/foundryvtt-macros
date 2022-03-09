0ae212f2414ff2a5c15152591f71c29d1f518229
xaukael <37848032+xaukael@users.noreply.github.com>
2022-03-09 09:30:21 -0600

    Create To Do.js

---
+++ To Do.js
@@ -0,0 +1,19 @@
+/*
+
+# To Do
+Actor Menu
+  header button for Actor Menu to toggle horizontal and vertical layout and set user flag for preference
+
+add images folder for screenshots of macros
+
+git download 
+  put into folder
+git update 
+  move to folder if not in one
+
+make these into a module...
+let macros = [...]
+for (let macro of macros)
+  toFile(macro.data.name+'.js') ... macro.command.replace("game.macros.getName(`macroName`).execute", `scope.functionName`, `${scope}.${macro.data.name.replace(' ', '')}`)
+
+*/
