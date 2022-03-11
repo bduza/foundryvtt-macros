1b27aaf62d0ee5639bc770f7341cba19700fd85d
xaukael <37848032+xaukael@users.noreply.github.com>
2022-03-11 11:51:02 -0600

    Update To Do.js

--- To Do.js
+++ To Do.js
@@ -27,12 +27,15 @@ for (let macro of macros)
   toFile(macro.data.name+'.js') ... macro.command.replace("game.macros.getName(`macroName`).execute", `scope.functionName`, `${scope}.${macro.data.name.replace(' ', '')}`)
 
 taskbar macro or just make a module
-  get hbs template file for app from github?
-  hide player and macro bar elements by id in style
-  re-populate open windows by hooking all application type renders?
-  start menu : populated from macro hotbar.
-  users in the notification area
-  copy calendar tool code from control bar
+  set add remove hooks at start, call them for each window on init (resolves code duplication problem
+  Shift Click a window to pin. 
+    get document data-uuid and save to attribute
+    if data-uuid attribute exists on creation, update that element id with the appId
+    then apply events
+    add pinned class: adds pin?
+    set user flag of current pins, just getting elements that have a data-uuid and adding them to an array of uuids
+    when taskbar loads, create elements NOT with addWindowToTaskbar, but a simple loop. Window information will be added if necessary
+      Do I need to check for a pinned window with that document uuid in addWindowToTaskbar? Probably
   taskbar color based on pause state?
   
 
