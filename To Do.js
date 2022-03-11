b204f1e057bd7b9457d5353a928005db51af02f2
xaukael <37848032+xaukael@users.noreply.github.com>
2022-03-11 13:46:29 -0600

    Update To Do.js

--- To Do.js
+++ To Do.js
@@ -2,20 +2,6 @@
 
 add images folder for screenshots of macros
 
-Actor Menu
-  header button for Actor Menu to toggle horizontal and vertical layout and set user flag for preference
-
-Spell Preparation
-  spell prep sets saved and retrieved from a object set in an actor flag
-  spellSets = spellSets ...  
-    actor.itemTypes.spells.filter(s=>s.data.data.preparation.mode === prepared && s.data.data.preparation.prepared).map(s=>return {_id:s.id, "data.preparation.prepared":s.data.data.preparation.prepared})
-    spellSets {
-      set1Name: [spell1.id, spell2.id, spell3.id],
-      set2Name: [spell4.id, spell5.id, spell6.id]
-    }
-  let prep = actor.itemTypes.spells.filter(s=>s.data.data.preparation.mode === prepared).map(s=>return {_id:s.id, "data.preparation.prepared":spellSet[set1Name].includes(s.id)});
-  actor.updateEmbeddedDocuments("Items", prep);
-
 git download 
   put into folder
 git update 
