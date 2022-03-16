40f113d7a385ebb7707f9fd0dd605e71e6ed5e19
xaukael <37848032+xaukael@users.noreply.github.com>
2022-03-16 08:35:43 -0500

    Update To Do.js

--- To Do.js
+++ To Do.js
@@ -12,17 +12,17 @@ let macros = [...]
 for (let macro of macros)
   toFile(macro.data.name+'.js') ... macro.command.replace("game.macros.getName(`macroName`).execute", `scope.functionName`, `${scope}.${macro.data.name.replace(' ', '')}`)
 
-taskbar macro or just make a module
-  set add remove hooks at start, call them for each window on init (resolves code duplication problem
-  Shift Click a window to pin. 
-    get document data-uuid and save to attribute
-    if data-uuid attribute exists on creation, update that element id with the appId
-    then apply events
-    add pinned class: adds pin?
-    set user flag of current pins, just getting elements that have a data-uuid and adding them to an array of uuids
-    when taskbar loads, create elements NOT with addWindowToTaskbar, but a simple loop. Window information will be added if necessary
-      Do I need to check for a pinned window with that document uuid in addWindowToTaskbar? Probably
-  taskbar color based on pause state?
+taskbar module/macro
+  check if pinned document exists when trying to pin it
+  if clicked and document is gone, remove
   
+5e Dialogs Module
+  actor menu header link to open raw character dialog
+  sidebar button for Chat Messages Dialog
+  hook to load css at start if setting
+  clicking token closes other actor menus and opens the selected token's (be sure to turn off click select token to avoid loops
+  add delay to mouseleave on dialogs similar to start menu
+  make a settings dialog to set mouseleave delay on user flag
+    
 
 */
