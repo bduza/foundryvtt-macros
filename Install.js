5241198a44ff4aea6c2c0b81e3f847546624b49f
xaukael <37848032+xaukael@users.noreply.github.com>
2022-04-12 13:16:20 -0500

    Update Install.js

--- Install.js
+++ Install.js
@@ -1,8 +1,4 @@
 let github = `https://raw.githubusercontent.com/xaukael/foundryvtt-macros/main/`;
-try {
 await jQuery.get(`${github}${encodeURI('Install Dialog')}.js`, async function(data) {
   eval(`( async function() { ${data} }())`);
 });
-} catch(error){
-  return ui.notifications.error('Macro not found on github: '+ error.responseText)
-}
