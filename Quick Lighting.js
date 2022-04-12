42ba45f11aa1eb9bb110b4f004e5d501ba4d7c41
xaukael <37848032+xaukael@users.noreply.github.com>
2022-04-12 11:41:12 -0500

    Add files via upload

--- Quick Lighting.js
+++ Quick Lighting.js
@@ -1,24 +1,28 @@
 let darkness = canvas.scene.data.darkness*100;
+let darknessColor =canvas.scene.data.flags['perfect-vision'].darknessColor || "#ffffff";
+let daylightColor =canvas.scene.data.flags['perfect-vision'].daylightColor || "#000000";
 new Dialog({
   title:'Scene Lighting',
   content:`
-    <form>
+    <form style="margin-top:-.5em;">
       <div class="form-group">
-        100 &nbsp;
- <input type="range" min="0" max="100" value="${darkness}" class="slider" name="inputField">
-        &nbsp;&nbsp;0
+        <input class="daylightColor" type="color" value="${daylightColor}" data-edit="tint" style="border:unset;"> 
+        &ensp; 100 &nbsp; <input type="range" min="0" max="100" value="${darkness}" class="darknessSlider" name="inputField">&nbsp;&nbsp;0 &nbsp;
+        <input class="darknessColor" type="color" value="${darknessColor}" data-edit="tint" style="border:unset;"><br>
       </div>
-    </form>`,
-  buttons:{
-    yes: {
-      icon: "<i class='fas fa-check'></i>",
-      label: `Apply Changes`
-    }},
-  default:'yes',
-  close: html => {
-    let result = html.find('input[name=\'inputField\']');
-    if (result.val()!== '') {
-        canvas.scene.update({ "darkness": (result.val()/100) },{ animateDarkness: false });
-	  }
-    }
+      </form>
+    `,
+  buttons: {},
+  render: (html) => {
+    html.find('.daylightColor, .darknessColor, .darknessSlider').change(async function(){
+      let darkness = html.find(`input.darknessSlider`).val()/100;
+      let darknessColor = html.find('input.darknessColor').val();
+      let daylightColor = html.find('input.daylightColor').val();
+      canvas.scene.update({ darkness , 
+      "flags.perfect-vision.darknessColor":darknessColor,
+      "flags.perfect-vision.daylightColor":daylightColor 
+      },{ animateDarkness: false });
+    });
+  },
+  close: (html) => {return}
 }).render(true);
