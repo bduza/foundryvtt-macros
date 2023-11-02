a50256cfa135de74477ca71e524e9cc88ddaf6e3
bduza <36064132+bduza@users.noreply.github.com>
2023-11-02 20:53:52 +1100

    Update Install.js

--- Install.js
+++ Install.js
@@ -1,4 +1,4 @@
-let github = `https://raw.githubusercontent.com/xaukael/foundryvtt-macros/main/`;
+let github = `https://raw.githubusercontent.com/bduza/foundryvtt-macros/main/`;
 await jQuery.get(`${github}${encodeURI('Install Dialog')}.js`, async function(data) {
   eval(`( async function() { ${data} }())`);
 });
